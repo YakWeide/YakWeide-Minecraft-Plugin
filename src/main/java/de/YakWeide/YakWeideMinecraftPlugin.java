e8b02b9f70d47b43997ad3f1c9760ae76502d54c
Tim Lisemer <tim.lisemer@gmx.de>
2021-12-20 21:28:47 +0100

    Basic Chat Api - Finished Basic Chat Api

--- src/main/java/de/YakWeide/YakWeideMinecraftPlugin.java
+++ src/main/java/de/YakWeide/YakWeideMinecraftPlugin.java
@@ -9,15 +9,15 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.ArrayList;
+import org.jetbrains.annotations.NotNull;
 
 public final class YakWeideMinecraftPlugin extends JavaPlugin {
 
-  private ChatApi chatApi = null;
 
   @Override
   public void onEnable() {
     getServer().getPluginManager().registerEvents(new Events(), this);
-    chatApi = new ChatApi(this);
+    Bukkit.getServer().getPluginManager().registerEvents(ChatApi.getInstance(), this);   //Register Chat Event
   }
 
   @Override
@@ -26,7 +26,7 @@ public final class YakWeideMinecraftPlugin extends JavaPlugin {
   }
 
 
-  public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+  public boolean onCommand(@NotNull CommandSender sender, @NotNull Command cmd, String label, String[] args) {
     if (label.equalsIgnoreCase("hello")) {
       if (sender instanceof Player) {
         Player player = (Player) sender;
@@ -49,10 +49,10 @@ public final class YakWeideMinecraftPlugin extends JavaPlugin {
       if(sender instanceof Player) {
         Player player = (Player) sender;
         String lastMessage = "";
-        if(chatApi.lastMessage(player) == null){
+        if(ChatApi.getInstance().lastMessage(player) == null){
           player.sendMessage("You haven sent a Message yet");
         }else {
-          lastMessage = chatApi.lastMessage(player);
+          lastMessage = ChatApi.getInstance().lastMessage(player);
         }
         player.sendMessage("Your Last Message was: " + lastMessage);
       }
--- src/main/java/de/YakWeide/chatApi/ChatApi.java
+++ src/main/java/de/YakWeide/chatApi/ChatApi.java
@@ -1,23 +1,29 @@
 package de.YakWeide.chatApi;
 
 import io.papermc.paper.event.player.AsyncChatEvent;
+import java.sql.Timestamp;
+import java.time.Instant;
 import java.util.HashMap;
 import java.util.UUID;
 import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.plugin.Plugin;
 
 public class ChatApi implements Listener {
 
-  private final HashMap<UUID, String> lastMessageHashMap = new HashMap<>();
-
-  public ChatApi(Plugin plugin) {
-    Bukkit.getServer().getPluginManager().registerEvents(this, plugin);
+  //Singleton --> https://www.geeksforgeeks.org/singleton-class-java/
+  private static ChatApi chatApi = null;
+  public static ChatApi getInstance()
+  {
+    if (chatApi == null)
+      chatApi = new ChatApi();
+    return chatApi;
   }
 
+  private final HashMap<UUID, String> lastMessageHashMap = new HashMap<>();
+
+  //Returns the last message of the given Player
   public String lastMessage(Player player){
     String lastMessage = lastMessageHashMap.get(player.getUniqueId());
     if(lastMessage == null || lastMessage.equals("")){
@@ -27,6 +33,7 @@ public class ChatApi implements Listener {
     }
   }
 
+  //Chat Event to log the player messages
   @EventHandler
   public void onPlayerChat(AsyncChatEvent event){
     Player p = event.getPlayer();
@@ -34,12 +41,24 @@ public class ChatApi implements Listener {
     lastMessageHashMap.put(p.getUniqueId(), Message);
   }
 
+  //Returns the next message of the given player, returns null after 60 seconds without a message
   public String nextMessage(Player player){
     String lastMessage = "";
     if(lastMessage(player) != null) lastMessage = lastMessage(player);
     player.sendMessage("Waiting for next Message...");
-    while(true){
-      if(!lastMessage(player).equals(lastMessage)) break;
+    Timestamp start = Timestamp.from(Instant.now());
+    while (lastMessage(player).equals(lastMessage)) {
+      Timestamp loop = Timestamp.from(Instant.now());
+
+      if (loop.getNanos() - start.getNanos() == 60000000)
+        return null; //Break after 60 seconds
+
+      try {
+        Thread.sleep(100);
+      } catch (InterruptedException ex) {
+        Thread.currentThread().interrupt();
+      }
+
     }
     return lastMessage(player);
   }
