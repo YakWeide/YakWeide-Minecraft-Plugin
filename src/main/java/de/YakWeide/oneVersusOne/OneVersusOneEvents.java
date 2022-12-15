6cc8e4b26863aaa56a661633994bd3a60d3502ce
Jan Reitz <jan_reitz@outlook.de>
2022-12-15 19:17:48 +0100

    OneVersusOne init

---
+++ src/main/java/de/YakWeide/oneVersusOne/OneVersusOneEvents.java
@@ -0,0 +1,6 @@
+package de.YakWeide.oneVersusOne;
+
+import org.bukkit.event.Listener;
+
+public class OneVersusOneEvents implements Listener {
+}
---
+++ src/main/java/de/YakWeide/oneVersusOne/OneVersusOneGames.java
@@ -0,0 +1,28 @@
+package de.YakWeide.oneVersusOne;
+
+import de.YakWeide.chatApi.ChatApi;
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+
+public class OneVersusOneGames {
+    private Player challenger;      //challenger
+    private Player challenged;      //challenged
+
+    private static ArrayList<Player> activeFighters = new ArrayList<>();
+    private final ChatApi chatApi = ChatApi.getInstance();
+
+
+    public OneVersusOneGames(Player challenger, Player challenged) {
+        this.challenger = challenger;
+        this.challenged = challenged;
+    }
+
+    public boolean begin(){
+
+
+    }
+
+
+
+}
